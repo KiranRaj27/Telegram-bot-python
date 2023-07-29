db377976f38ac05156804689205bb0c1cff5b310
Kiran Raj <92741173+KiranRaj27@users.noreply.github.com>
2023-07-29 22:44:20 +0530

    first commit

---
+++ bot.py
@@ -0,0 +1,49 @@
+# import os
+import requests
+import telebot
+
+BOT_TOKEN = ""
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=["start","hello"])
+def send_welcome(message):
+    bot.reply_to(message,"Howdy how are you doing")
+
+
+@bot.message_handler(func=lambda msg:True)
+def echo_all(message):
+    bot.reply_to(message,message.text)
+
+@bot.message_handler(commands=['horoscope'])
+def sign_handler(message):
+    text = "What's your zodiac sign?\nChoose one: *Aries*, *Taurus*, *Gemini*, *Cancer,* *Leo*, *Virgo*, *Libra*, *Scorpio*, *Sagittarius*, *Capricorn*, *Aquarius*, and *Pisces*."
+    sent_msg = bot.send_message(message.chat.id, text, parse_mode="Markdown")
+    bot.register_next_step_handler(sent_msg,day_handler)
+
+bot.infinity_polling()
+
+
+
+def get_daily_horoscope(sign:str,day:str) ->dict:
+    url = "https://horoscope-app-api.vercel.app/api/v1/get-horoscope/daily"
+    params = {"sign":sign,"day":day}
+    response = requests.get(url,params)
+
+    return response.json()
+
+def fetch_horoscope(message, sign):
+    day = message.text
+    horoscope = get_daily_horoscope(sign, day)
+    data = horoscope["data"]
+    horoscope_message = f'*Horoscope:* {data["horoscope_data"]}\\n*Sign:* {sign}\\n*Day:* {data["date"]}'
+    bot.send_message(message.chat.id, "Here's your horoscope!")
+    bot.send_message(message.chat.id, horoscope_message, parse_mode="Markdown")
+
+def day_handler(message):
+    sign = message.text
+    text = "What day do you want to know?\nChoose one: *TODAY*, *TOMORROW*, *YESTERDAY*, or a date in format YYYY-MM-DD."
+    sent_msg = bot.send_message(
+        message.chat.id, text, parse_mode="Markdown")
+    bot.register_next_step_handler(
+        sent_msg, fetch_horoscope, sign.capitalize())
